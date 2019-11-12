f2248250076ff2dc1078e78a424419ed291adce5
xtenzQ <rusetscky@outlook.com>
2019-11-13 00:46:04 +0900

    lab2.c rewritten

--- Lab 2/lab2.c
+++ Lab 2/lab2.c
@@ -2,71 +2,69 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Displays error
+ *
+ * @param  conn  database connection
+ * @param  query applied query/
+ */
+void display_error(MYSQL *conn, char *query) {
+        if (mysql_query(conn, query)) {
+                fprintf(stderr, "%s\n", mysql_error(conn));
+                exit(1);
+        }
+}
+
+/**
+ * Inserts new student with given parameters into table "student"
+ *
+ * @param  conn    database connection
+ * @param  id      primary key column
+ * @param  name    name column
+ * @param  dept    dept_name column
+ * @param  credits credits column 
+ */
 void insert_new_student(MYSQL *conn, char *id, char *name, char *dept, int credits) {
         char *temp_query = "INSERT INTO student VALUES ('%s', '%s', '%s', %d);";
         char *query = (char *) malloc(strlen(temp_query) + 20);
         sprintf(query, temp_query, id, name, dept, credits);
-
-        if (mysql_query(conn, query)) {
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                exit(1);
-        }
-
-        //res = mysql_use_result(conn);
+        display_error(conn, query);
         free(query);
 }
 
+/**
+ * Delete student with given id from given table
+ *
+ * @param  conn       database connection
+ * @param  table_name name of the table in database
+ * @param  id         student id
+ */
 void delete_by_id(MYSQL *conn, char *table_name, char *id) {
         char *temp_query = "DELETE FROM %s WHERE ID=%s;";
         char *query = (char *) malloc(strlen(temp_query) + 20);
         sprintf(query, temp_query, table_name, id);
-
-        if (mysql_query(conn, query)) {
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                exit(1);
-        }
-
-        //res = mysql_use_result(conn);
+        display_error(conn, query);
         free(query);
 }
 
-int main(){
-        MYSQL *conn;
+/**
+ * Displays student with given name
+ *
+ * @param  conn database connection
+ * @param  name student name
+ * @param  id   student id
+ */
+void display_results(MYSQL *conn, char *name) {
         MYSQL_RES *res;
         MYSQL_ROW row;
-
-        char *server = "localhost";
-        char *user = "group4";
-        char *password = "12345678";
-        char *database = "group4_university_db";
-
-        conn = mysql_init(NULL);
-
-        /*Connect to the database*/
-
-        if(!mysql_real_connect(conn, server, user, password, database, 0, NULL,0)){
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                exit(1);
-        }
-
-        //insert_new_student(conn, "1001", "Nikita", "Comp. Sci.", 3);
-        delete_by_id(conn, "student", "1001");
-        //res = mysql_use_result(conn);
-
-        char *temp_query = "SELECT * FROM student WHERE name='Nikita';";
+        char *temp_query = "SELECT * FROM student WHERE name='%s';";
         char *query = (char *) malloc(strlen(temp_query) + 20);
-        sprintf(query, temp_query);
-
-        /*send SQL query*/
-        if(mysql_query(conn, query)) {
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                exit(1);
-        }
-
+        sprintf(query, temp_query, name);
+        display_error(conn, query);
         res = mysql_use_result(conn);
         free(query);
 
-        /*Output table name*/
+        // Output table name
         printf("\n\n * * * * Result of query * * * * \n\n");
         puts("_______________________________________________________");
         puts("");
@@ -75,7 +73,29 @@ int main(){
                 printf("%10s | %30s | %20s | %10s\n", row[0], row[1], row[2], row[3]);
         puts("_______________________________________________________");
 
-        /*close connection */
+        // Close connection
         mysql_free_result(res);
+}
+
+int main() {
+        MYSQL *conn;
+
+        char *server = "localhost";
+        char *user = "group4";
+        char *password = "12345678";
+        char *database = "group4_university_db";
+
+        conn = mysql_init(NULL);
+
+        // Connect to the database
+        if(!mysql_real_connect(conn, server, user, password, database, 0, NULL,0)){
+                fprintf(stderr, "%s\n", mysql_error(conn));
+                exit(1);
+        }
+
+        insert_new_student(conn, "1003", "Nikita", "Comp. Sci.", 3);
+        delete_by_id(conn, "student", "1003");
+        display_results(conn, "Nikita");
+
         mysql_close(conn);
 }
